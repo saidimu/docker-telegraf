2c474c2e29696c09fe051f5524cb89d1f40a2f83
saidimu apale <saidimu@gmail.com>
2015-12-08 18:11:46 -0500

    Updated to match v0.2.4 config file
    https://github.com/influxdb/telegraf/blob/0.2.4/etc/telegraf.conf

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ if [ "${INFLUXDB_HOST}" == "**None**" ]; then
 fi
 
 if [ -n "${INFLUXDB_HOST}" ]; then
-  sed -i -u "s#url = \"localhost\"#url = \"http://${INFLUXDB_HOST}:8086\"#g" ${CONFIG_FILE}
+  sed -i -u "s#urls = [\"http://localhost:8086\"]#urls = [\"http://${INFLUXDB_HOST}:8086\"]#g" ${CONFIG_FILE}
 fi
 
 echo "=> Starting Telegraf ..."
